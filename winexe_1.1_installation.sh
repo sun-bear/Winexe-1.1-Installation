5bb54125bf951d137f923bf8dd00b97a933479b6
sun-bear <mscheler@zoho.com>
2020-08-19 04:09:35 -0500

    Update winexe_1.1_installation.sh

--- winexe_1.1_installation.sh
+++ winexe_1.1_installation.sh
@@ -1,6 +1,3 @@
-#Tolga AKKAPULU
-# /tolgaakkapulu
-
 #!/bin/bash
 
 echo -e "\e[31m+-+-+ WINEXE 1.1 Installation +-+-+\e[0m\n"
@@ -8,14 +5,6 @@ sleep 1
 echo -e "\e[1;34msudo apt-get -y install gcc-mingw-w64 comerr-dev libpopt-dev libbsd-dev zlib1g-dev libc6-dev python-dev libacl1-dev libldap2-dev git\e[0m"
 sleep 1
 sudo apt-get -y install gcc-mingw-w64 comerr-dev libpopt-dev libbsd-dev zlib1g-dev libc6-dev python-dev libacl1-dev libldap2-dev git
-echo -e "\e[1;34mtar zxvf samba-4.1.23.tar.gz\e[0m"
-sleep 1
-tar zxvf samba-4.1.23.tar.gz
-sudo chmod +x -R . && \
-echo -e "\e[1;34mtar zxvf winexe-winexe-waf.tar.gz\e[0m"
-sleep 1
-tar zxvf winexe-winexe-waf.tar.gz
-sudo chmod +x -R . && \
 
 echo -e "\e[1;34mcd winexe-winexe-waf/source\e[0m"
 sleep 1
@@ -33,7 +22,7 @@ sleep 1
 echo -e "\e[1;34msudo cp build/winexe-static /usr/local/bin/winexe\e[0m"
 sleep 1
 sudo cp build/winexe-static /usr/local/bin/winexe
-sudo chmod +x -R . && \
 
 echo -e "\n\e[31mUsage : \e[0m\e[1;34mwinexe -U domain/username%password //ip_address 'command'\e[0m"
 echo -e "\n\e[31m+-+-+ WINEXE 1.1 Installation Complete +-+-+\e[0m"
+
