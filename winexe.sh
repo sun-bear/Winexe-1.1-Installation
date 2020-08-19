aa9880fe68d6aa515f8bf2af4cb39e5a354552fc
sun-bear <mscheler@zoho.com>
2020-08-19 07:15:22 -0500

    Update winexe.sh

--- winexe.sh
+++ winexe.sh
@@ -2,7 +2,6 @@
 
 echo -e "\e[31m+-+-+ WINEXE 1.1 Installation +-+-+\e[0m\n"
 sleep 1
-sudo apt-get -y install python2.7 libtevent-dev
-sudo apt-get unstable install samba-dev
+sudo apt-get -y install python2.7 libtevent-dev samba-dev
 cd source
 
